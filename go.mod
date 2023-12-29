477023ca3e1325486b76f708f41391fca48357f3
pop86 <chaitankarp86@gmail.com>
2023-12-29 19:23:36 +0530

    Added new func BigBarks

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/pop96/puppy
 
 go 1.21.2
+
+require github.com/pop96/dog v0.0.0-20231227144251-43d731a19340
--- puppy.go
+++ puppy.go
@@ -1,9 +1,21 @@
 package puppy
 
+import (
+	"github.com/pop96/dog"
+)
+
 func Bark() string {
 	return "Wooh!!"
 }
 
 func Barks() string {
 	return "Wooh, Wooh, Wooh!!"
+}
+
+func BigBark() string {
+	return dog.WhenGrownUp(Bark())
+}
+
+func BigBarks() string {
+	return dog.WhenGrownUp(Barks())
 }
